cf770fc3c20b1c2044ba482522bca4318a8f72a5
danosdim <danosdimitris@gmail.com>
2020-06-05 15:31:13 +0300

    fixed depth

--- enum.sh
+++ enum.sh
@@ -45,27 +45,27 @@ file_specification() {
     printf "%*s\tName only:\t${YEL}%s${NCL}\n" $((indent + 4)) '' "$NAME"
     printf "%*s\tExtension:\t${YEL}%s${NCL}\n" $((indent + 4)) '' "$EXT"
     printf "%*s\tFile size:\t${YEL}%s${NCL}\n" $((indent + 4)) '' "$SIZE"
-    
+
     if [ $ATTR == "755" ]; then
         ATTR_755=$((ATTR_755 + 1))
-        elif [ $ATTR == "777" ]; then
+    elif [ $ATTR == "777" ]; then
         ATTR_777=$((ATTR_777 + 1))
         printf "\t\t${RED}[IMPORTANT!]${YEL} This file has 777 attributes -> ${RED}$file \n"
-        elif [ $ATTR == "664" ]; then
+    elif [ $ATTR == "664" ]; then
         ATTR_664=$((ATTR_664 + 1))
-        elif [ $ATTR == "655" ]; then
+    elif [ $ATTR == "655" ]; then
         ATTR_655=$((ATTR_655 + 1))
     else
         OTH_ATTR=$((OTH_ATTR + 1))
-        
+
     fi
-    
+
 }
 
 walk() {
     local indent="${2:-0}"
     printf "\n%*s${RED}%s${NCL}\n\n" "$indent" '' "$1"
-    
+
     # If the entry is a file do some operations
     for entry in "$1"/*; do
         if [[ -f "$entry" ]]; then
@@ -73,11 +73,10 @@ walk() {
         fi
     done
     # If the entry is a directory call walk() == create recursion
-    
+
     for entry in "$1"/*; do
         if [[ -d "$entry" ]]; then
             DIR_DEPTH=$(echo "$entry" | tr " /" "- " | wc -w)
-            ##!IMPORTANT CHANGE THE COMPARISON TO BE COMPATIBLE
             if [[ "$DIR_DEPTH" -le "$DEPTH" ]]; then
                 walk "$entry" $((indent + 4))
                 DIR_NUM=$((DIR_NUM + 1))
@@ -87,7 +86,7 @@ walk() {
 }
 
 file_permissions() {
-    
+
     printf "\n\t\t\t\t\t${RED}File Permissions!${NCL}\n\n"
     printf "\n\t\t\t${BLU}===================================================================${NCL}\n\n"
     #ATTR_*644=$(( ( $ATTR_*644 / FILE_NUM ) * 100 ))
@@ -96,23 +95,23 @@ file_permissions() {
     perc_655=$((ATTR_655 * 100 / FILE_NUM))
     perc_664=$((ATTR_664 * 100 / FILE_NUM))
     perc_other=$((OTH_ATTR * 100 / FILE_NUM))
-    
+
     printf "\n\n\t\t\t\t${YEL}Total Files\t777\t755\t655\t664\tOther${NCL}\n\n"
     printf "\t\t\t\t${GRE}$FILE_NUM\t\t$ATTR_777\t$ATTR_755\t$ATTR_655\t$ATTR_664\t$OTH_ATTR\n\n"
     printf "\t\t\t\t${GRE}100 %% \t\t$perc_777%%\t$perc_755%%\t$perc_655%%\t$perc_664%%\t$perc_other%% ${NCL}\n\n"
-    
+
 }
 
 lastly_created() {
     printf "\n\n\t\t\t\t\t${RED}Five lastly created files!${NCL}\n\n"
     printf "\n\t\t\t${BLU}===================================================================${NCL}\n\n"
     # printf "\n\t\t\t${GRE}Time\t\tMonth\t\Name${YEL}\n"
-    
+
     for i in {2..6}; do
         ls -tl | head -6 | awk '{print $9}' | sed "${i}q;d"
         ls -tl | head -6 | awk '{print $8}' | sed "${i}q;d"
         ls -tl | head -6 | awk '{print $7}' | sed "${i}q;d"
-        
+
         printf "${BLU}========================\n${NCL}"
     done
 }
@@ -128,7 +127,7 @@ dir_files() {
     printf "\n\n\t\t\t\t\t${RED}Directories with most files!${NCL}\n\n"
     printf "\n\t\t\t${BLU}===================================================================${NCL}\n\n"
     printf "\n\t\t${GRE}    Number of files\t\t${GRE}Absolute Paths to the directories${NCL}\n\n"
-    
+
     find ${1} -maxdepth ${2} -type d | while read -r dir; do
         NUM=$(ls "$dir" -lAh | grep -v 'd' | wc -l)
         [ -d "$dir" ] && printf "\n\t\t\t${NCL}${YEL} $NUM\t\t%s\t" "$dir"
@@ -139,7 +138,7 @@ dir_directories() {
     printf "\n\n\t\t\t\t\t${RED}Directories with most directories!${NCL}\n\n"
     printf "\n\t\t\t${BLU}===================================================================${NCL}\n\n"
     printf "\n\t\t${GRE}    Number of dirs\t\t${GRE}Absolute Paths to the directories${NCL}\n\n"
-    
+
     find ${1} -maxdepth ${2} -type d | while read -r dir; do
         NUM=$(ls "$dir" -lAh | grep ^d | wc -l)
         [ -d "$dir" ] && printf "\n\t\t\t${NCL}${YEL} $NUM\t\t%s\t" "$dir"
@@ -150,7 +149,7 @@ dir_size() {
     printf "\n\n\t\t\t\t\t${RED}Directories with largest size!${NCL}\n\n"
     printf "\n\t\t\t${BLU}===================================================================${NCL}\n\n"
     printf "\n\t\t${GRE}    Size of directories\t\t${GRE}Absolute Paths to the directories${NCL}\n\n"
-    
+
     find ${1} -maxdepth ${2} -type d | while read -r dir; do
         NUM=$(du -h -d 1 "$dir")
         [ -d "$dir" ] && printf "\n\t\t\t${YEL} $NUM\t\t%s:\t ${NCL}" "$dir"
@@ -161,21 +160,21 @@ dir_size() {
 # as a second parameter the depth of the look is specified
 
 lastly_modified() {
-    
+
     printf "\n\n\t\t\t\t\t${RED}Five lastly modified files!${NCL}\n\n"
     printf "\n\t\t\t${BLU}===================================================================${NCL}\n\n"
     printf "\n\t\t\t${GRE}Time\t\tEpoch Time\t\tAbsolute Path${YEL}\n"
-    
+
     find ${1} -maxdepth ${2} -not -path '*/\.*' -type f -exec stat --format ':%y %n' "{}" \; | sort -nr | cut -d: -f2- | head -5 | awk '{ printf "\n\t\t\t"$1"\t"$2"\t"$4"\t"}'
-    
+
 }
 
 lastly_accessed() {
-    
+
     printf "\n\n\t\t\t\t\t${RED}Five lastly accessed files!${NCL}\n\n"
     printf "\n\t\t\t${BLU}===================================================================${NCL}\n\n"
     printf "\n\t\t\t${GRE}Time\t\tEpoch Time\t\tAbsolute Path${YEL}\n"
-    
+
     find ${1} -maxdepth ${2} -not -path '*/\.*' -type f -exec stat --format ':%x %n' "{}" \; | sort -nr | cut -d: -f2- | head -5 | awk '{ printf "\n\t\t\t"$1"\t"$2"\t"$4"\t"}'
 }
 
@@ -187,32 +186,34 @@ system_info() {
     if [ "$unameinfo" ]; then
         printf "\n\n\t\t\t${GRE}Kernel information:${YEL}\t\t $unameinfo \n"
     fi
-    
+
     release=$(cat /etc/*-release 2>/dev/null)
     if [ "$release" ]; then
         printf "\n\t\t\t${GRE}Specific release information:${YEL}\n"
-        
+
         for each in $release; do
             printf "\n\t\t\t $each"
         done
-        
+
     fi
-    
+
     hostname=$(hostname 2>/dev/null)
     if [ "$hostname" ]; then
         printf "\n\n\t\t\t${GRE}Hostname:${YEL}\t\t $hostname \n"
-        
+
     fi
     printf "\n\n\n\t\t\t\t\t${RED}Disk Sizes!${NCL}\n\n"
     printf "\n\t\t\t${BLU}===================================================================${NCL}\n\n"
     printf "\t\t\t\t${YEL}Disk Sector\tSize\tUsed\tAvailable\tPercentage${NCL}\n"
     df -h | grep "/dev/sda*" | awk '{ printf "\n\t\t\t\t"$1"\t\033[1;34m" $2"\t\033[0;31m"$3"\t\033[1;32m"$4"\t\t\033[0;31m"$5"\033[0m"}'
-    
+
 }
 # ENTRY POINT
 # If the path is empty use the current, otherwise convert relative to absolute; Exec walk()
 if [[ -z "${1}" ]]; then
-ABS_PATH="${PWD}" || cd "${1}" && ABS_PATH="${PWD}"
+    ABS_PATH="${PWD}"
+else
+    cd "${1}" && ABS_PATH="${PWD}"
 fi
 
 if [[ -z "${2}" ]]; then
@@ -220,6 +221,9 @@ if [[ -z "${2}" ]]; then
 else
     DEPTH="${2}"
 fi
+
+LIMIT=$(echo "$ABS_PATH" | tr " /" "- " | wc -w)
+DEPTH=$(( DEPTH + LIMIT ))
 
 walk "${ABS_PATH}"
 total_stats "${ABS_PATH}" "${DEPTH}"
@@ -236,6 +240,3 @@ system_info
 # printf "\n\t\t\t${BLU}===================================================================${NCL}\n\n"
 
 printf "\n\n\n\n\t\t\t\t${RED} Execution finished !!!${NCL}\n"
-printf "\n\t\t\t\t${RED} Execution finished !!!${NCL}\n"
-printf "\n\t\t\t\t${RED} Execution finished !!!${NCL}\n"
-printf "\n\t\t\t\t${RED} Execution finished !!!${NCL}\n\n\n"
